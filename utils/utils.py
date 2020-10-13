6747cbf05c1485aa1cbdbf99faaa3635098706e6
Michał Jaworski <jawor403@gmail.com>
2020-10-13 22:46:19 +0200

    Configuration reading

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,16 @@
+from typing import Tuple
+
+import yaml
+from dotmap import DotMap
+
+
 def plot_sample(ax, sample):
     ax.imshow(sample[0].detach(), cmap="gray", vmin=-1, vmax=1)
+
+
+def read_config(path: str) -> Tuple[DotMap, str]:
+    with open(path) as f:
+        src = f.read()
+    d = yaml.safe_load(src)
+    d = DotMap(d)
+    return d, src
