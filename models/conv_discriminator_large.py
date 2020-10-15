83238bbc8582fa7f27aeac2aecf1b6593ad1038e
Michał Jaworski <jawor403@gmail.com>
2020-10-15 18:10:50 +0200

    Update configurations

--- models/conv_discriminator_large.py
+++ models/conv_discriminator_large.py
@@ -9,18 +9,17 @@ class ConvDiscriminator(BaseDiscriminator):
         super().__init__(config)
         p_dropout = config.discriminator.get("dropout", 0)
         self.layers = nn.Sequential(
-            nn.Conv2d(1, 64, 5),  # 64 x 24 x 24
+            nn.Conv2d(1, 32, 3),  # 32 x 26 x 26
             nn.ReLU(),
             nn.Dropout(p_dropout),
-            nn.Conv2d(64, 64, 5),  # 64 x 20 x 20
-            nn.ReLU(),
-            nn.MaxPool2d(2),  # 64 x 10 x 10
+            nn.Conv2d(32, 32, 3),  # 32 x 24 x 24
+            nn.MaxPool2d(2),  # 32 x 12 x 12
             nn.Dropout(p_dropout),
-            nn.Conv2d(64, 128, 5),  # 128 x 6 x 6
+            nn.Conv2d(32, 64, 5),  # 64 x 8 x 8
             nn.ReLU(),
             nn.Dropout(p_dropout),
             nn.Flatten(),
-            nn.Linear(128 * 6 * 6, 1),
+            nn.Linear(64 * 8 * 8, 1),
             nn.Sigmoid(),
         )
 
